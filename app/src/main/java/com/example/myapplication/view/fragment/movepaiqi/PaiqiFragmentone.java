00a3882c0011feb497a449835c2c712fef7dbb9d
liubingliang1 <347109180@qq.com>
2019-11-09 10:59:05 +0800

    推荐影院11.7第一次push

--- app/src/main/java/com/example/myapplication/view/fragment/movepaiqi/PaiqiFragmentone.java
+++ app/src/main/java/com/example/myapplication/view/fragment/movepaiqi/PaiqiFragmentone.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +16,26 @@ import com.example.myapplication.view.inteface.MomInteface;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /*
  *@Auther:刘炳良
  *@Date: 时间
  *@Description:功能
  * */public class PaiqiFragmentone extends Fragment implements MomInteface.dopaiqi {
 
+    @BindView(R.id.paiqi)
+    RecyclerView paiqi;
+    Unbinder unbinder;
     private List<PaiqiBean.ResultBean> result;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View inflate = inflater.inflate(R.layout.paiqilayoutone, container, false);
+        unbinder = ButterKnife.bind(this, inflate);
         return inflate;
     }
 
@@ -36,6 +46,8 @@ import java.util.List;
     }
 
     private void initview() {
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        paiqi.setLayoutManager(manager);
 
     }
 
@@ -43,5 +55,11 @@ import java.util.List;
     public void success(PaiqiBean bean) {
         result = bean.getResult();
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
